d9bbc9addabb38bbac4db5e2e089aac65ed1d08e
Aaron McCollum <mccollumaarons@gmail.com>
2022-03-12 08:19:59 +0900

    added word length answer

--- intro_to_programming/hashes.rb
+++ intro_to_programming/hashes.rb
@@ -9,3 +9,15 @@ end
 def get_full_name(hash)
     hash["first"] + " " + hash["last"] + ", the " + hash["title"]
 end
+
+
+# Write a method word_lengths that takes in a sentence string and returns a hash where every key is a word
+# of the sentence, and its' corresponding value is the length of that word.
+def word_lengths(sentence)
+    words = sentence.split(" ")
+    words_hash = Hash.new(0)
+    
+    words.each { |word| words_hash[word] = word.length }
+    
+    words_hash
+end
